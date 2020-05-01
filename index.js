2c1ed67ee6879e5d61ae26a226e73c79b7da750c
LinnKlaas <linn.klaas@hfg-gmuend.de>
2020-05-01 13:34:23 +0200

    update

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const axios = require('axios');
+// Imports
+const axios = require('axios').default;
 const express = require('express');
+
+// Library inits
 const app = express();
 
-const url = 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json';
+let data = '';
 
-axios.get(url)
-  .then(function callback(body) {
-    console.log(body);
+axios({
+  method: 'get',
+  url: 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json',
+  responseType: 'json',
+})
+  .then((response) => {
+    const output = response.data;
+    // eslint-disable-next-line no-console
+    console.log(output);
+    data = output;
   });
+
+
+// Params - REST-artig
+app.get('/user/:uid', (req, res) => {
+  res.send(`User ID is set to ${req.params.uid}`);
+  // tu was
+});
 
 app.get('/mensa/:day', (req, res) => {
   if (req.params.day === 'Di') {
