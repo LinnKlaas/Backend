7cdb161f72fcec8669c82e3336b88f34338f9fc0
LinnKlaas <linn.klaas@hfg-gmuend.de>
2020-05-06 17:27:41 +0200

    api change

--- index.js
+++ index.js
@@ -4,35 +4,45 @@ const express = require('express');
 
 // Library inits
 const app = express();
+app.use(express.json());
 
 let data = '';
+const uri = 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json';
 
-axios({
-  method: 'get',
-  url: 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json',
-  responseType: 'json',
-})
-  .then((response) => {
-    const output = response.data;
-    // eslint-disable-next-line no-console
-    console.log(output);
-    data = output;
-  });
-
-
-// Params - REST-artig
-app.get('/user/:uid', (req, res) => {
-  res.send(`User ID is set to ${req.params.uid}`);
-  // tu was
-});
+async function getData() {
+  await axios.get(uri)
+    .then((req) => {
+      data = req.data;
+    })
+    .catch(() => {
+      data = undefined;
+    });
+}
+getData();
 
 app.get('/mensa/:day', (req, res) => {
-  if (req.params.day === 'Di') {
-    res.send(data);
+  if (data !== undefined) {
+    if (req.params.day === 'Di') {
+      res.send(data);
+    } else {
+      res.status(404).send('Error: 404');
+    }
   } else {
-    res.status(404).send('404');
+    res.status(404).send('Error: 404');
   }
-  // tu was
+});
+
+app.post('/api/addData/', (req, res) => {
+  if (!JSON.stringify(data).includes(JSON.stringify(req.body))) { //macht String draus, include vergleicht
+    data.push(req.body);
+    res.status(200).send();
+  } else {
+    res.status(418).send();
+  }
+});
+
+app.get('/api/getData/', (req, res) => {
+  res.status(200).send(data);
 });
 
 // Server starten
